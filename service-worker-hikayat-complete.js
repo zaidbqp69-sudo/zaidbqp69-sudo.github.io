353eb276d32c711199ca1be1d8c206730bcb47ad
zaidbqp69-sudo <3.28832743e+08+zaidbqp69-sudo@users.noreply.github.com>
2026-09-26 15:41:48 +0000

    Remove B1 stage fright story

--- service-worker-hikayat-complete.js
+++ service-worker-hikayat-complete.js
@@ -1,5 +1,5 @@
 /* talevo offline Service Worker */
-const VERSION = 'talevo-offline-v16';
+const VERSION = 'talevo-offline-v17';
 const APP_CACHE = `${VERSION}-app`;
 const STORY_CACHE = 'talevo-stories';
 const APP_SHELL = [
